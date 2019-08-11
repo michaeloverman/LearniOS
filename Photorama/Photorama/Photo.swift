88f9a10c1a6239f0f8d2921927bcce59506956e6
Michael Overman <overman.michael@gmail.com>
2019-08-11 06:27:52 -0600

    Photorama through Ch 20

---
+++ Photorama/Photorama/Photo.swift
@@ -0,0 +1,24 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Michael Overman on 8/10/19.
+//  Copyright © 2019 Michael Overman. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+    
+    init(title: String, photoID: String, remoteURL: URL, dateTaken: Date) {
+        self.title = title
+        self.photoID = photoID
+        self.remoteURL = remoteURL
+        self.dateTaken = dateTaken
+    }
+}
