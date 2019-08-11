fdf1401c8e97c2a1e3f44cbb4c7e0faa86961da6
Michael Overman <overman.michael@gmail.com>
2019-08-10 20:04:29 -0600

    Photorama initial commit

---
+++ Photorama/Photorama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Michael Overman on 8/10/19.
+//  Copyright © 2019 Michael Overman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
