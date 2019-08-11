59d87e7f27f0aeca2b833a8901fe037d919353f1
Michael Overman <overman.michael@gmail.com>
2019-08-10 20:04:29 -0600

    Photorama initial commit

---
+++ Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Michael Overman on 8/10/19.
+//  Copyright © 2019 Michael Overman. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
