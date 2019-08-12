1b8ceb25ac5e3c240bd0702c508276d0e2f9faab
Michael Overman <overman.michael@gmail.com>
2019-08-11 18:15:08 -0600

    CoreData persistence implement, ch 22

---
+++ Photorama/Photorama/Photo+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Photo+CoreDataClass.swift
+//  Photorama
+//
+//  Created by Michael Overman on 8/11/19.
+//  Copyright © 2019 Michael Overman. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
--- Photorama/Photorama/PhotosViewController.swift
+++ Photorama/Photorama/PhotosViewController.swift
@@ -22,21 +22,25 @@ class PhotosViewController: UIViewController, UICollectionViewDelegate {
         collectionView.dataSource = photoDataSource
         collectionView.delegate = self
         
+        updateDataSource()
+        
         store.fetchInterestingPhotos {
             (photosResult) -> Void in
             
-            switch photosResult {
-            case let .success(photos):
-                print("Successfully found \(photos.count) photos.");
-//                if let firstPhoto = photos.first {
-//                    self.updateImageView(for: firstPhoto)
-//                }
-                self.photoDataSource.photos = photos
-            case let .failure(error):
-                print("Error fetching interesting photos: \(error)")
-                self.photoDataSource.photos.removeAll()
-            }
-            self.collectionView.reloadSections(IndexSet(integer: 0))
+//            switch photosResult {
+//            case let .success(photos):
+//                print("Successfully found \(photos.count) photos.");
+////                if let firstPhoto = photos.first {
+////                    self.updateImageView(for: firstPhoto)
+////                }
+//                self.photoDataSource.photos = photos
+//            case let .failure(error):
+//                print("Error fetching interesting photos: \(error)")
+//                self.photoDataSource.photos.removeAll()
+//            }
+//            self.collectionView.reloadSections(IndexSet(integer: 0))
+            
+            self.updateDataSource()
         }
     }
     
@@ -87,6 +91,20 @@ class PhotosViewController: UIViewController, UICollectionViewDelegate {
             }
         default:
             preconditionFailure("Unexpected segue identifier.")
+        }
+    }
+    
+    private func updateDataSource() {
+        store.fetchAllPhotos {
+            (photosResult) in
+            
+            switch photosResult {
+            case let .success(photos):
+                self.photoDataSource.photos = photos
+            case .failure:
+                self.photoDataSource.photos.removeAll()
+            }
+            self.collectionView.reloadSections(IndexSet(integer: 0))
         }
     }
     
