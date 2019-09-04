cad831494339951b5dcff43bafa6501c1eedcacd
Michael Overman <overman.michael@gmail.com>
2019-09-03 23:59:52 -0400

    Beginning of ErrorHandling - not finished

---
+++ Swift/ErrorHandling.playground/Contents.swift
@@ -0,0 +1,108 @@
+import Cocoa
+
+enum Token {
+    case Number(Int)
+    case Plus
+}
+
+class Lexer {
+    enum Error: Swift.Error {
+        case InvalidCharacter(Character)
+    }
+    
+    let input: String
+    var position: String.Index
+    
+    init(input: String) {
+        self.input = input
+        self.position = self.input.startIndex
+    }
+    
+    func peek() -> Character? {
+        guard position < input.endIndex else {
+            return nil
+        }
+        return input[position]
+    }
+    
+    func advance() {
+        assert(position < input.endIndex, "Cannot advance past the end!")
+        position = input.index(after: position)
+    }
+    
+    func getNumber() -> Int {
+        var value = 0
+        
+        while let nextCharacter = peek() {
+            switch nextCharacter {
+            case "0" ... "9":
+                // Another digit - add it into value
+                let digitValue = Int(String(nextCharacter))!
+                value = 10*value + digitValue
+                advance()
+                
+            default:
+                // A non-digit - go back to regular indexing
+                return value
+            }
+        }
+        
+        return value
+    }
+    
+    func lex() throws -> [Token] {
+        var tokens = [Token]()
+        
+        while let nextCharacter = peek() {
+            switch nextCharacter {
+            case "0" ... "9":
+                // Start of a number - need to grab the rest
+                let value = getNumber()
+                tokens.append(.Number(value))
+            case "+":
+                tokens.append(.Plus)
+                advance()
+                
+            case " ":
+                // Just advance to ignore spaces
+                advance()
+                
+            default:
+                // Something unexpected - need to send back an error
+                throw Error.InvalidCharacter(nextCharacter)
+            }
+        }
+        
+        return tokens
+    }
+}
+
+class Parser {
+    let tokens: [Token]
+    var position = 0
+    
+    init(tokens: [Token]) {
+        self.tokens = tokens
+    }
+    
+    func getNextToken() -> Token? {
+        guard position < tokens.count else {
+            return nil
+        }
+        return tokens[position++]
+    }
+}
+
+func evaluate(input: String) {
+    print("Evaluating: \(input)")
+    let lexer = Lexer(input: input)
+    do {
+        let tokens = try lexer.lex()
+        print("Lexer output: \(tokens)")
+    } catch {
+        print("An error occured: \(error)")
+    }
+}
+
+evaluate(input: "10 + 3 + 5")
+//evaluate(input: "1 + 2 + abcdefg")
