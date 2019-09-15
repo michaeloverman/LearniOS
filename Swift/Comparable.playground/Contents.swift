c30a2c2f05915693d41463226f9a72df9ba9b34a
Michael Overman <overman.michael@gmail.com>
2019-09-15 15:12:05 -0600

    Ch 25 comparable and equatable

---
+++ Swift/Comparable.playground/Contents.swift
@@ -0,0 +1,36 @@
+import Cocoa
+
+struct Point: Comparable {
+    let x: Int
+    let y: Int
+}
+
+//func ==(lhs: Point, rhs: Point) -> Bool {
+//    return (lhs.x == rhs.x) && (lhs.y == rhs.y)
+//}
+
+func <(lhs: Point, rhs: Point) -> Bool {
+    return (lhs.x < rhs.x) && (lhs.y < rhs.y)
+}
+
+func +(lhs: Point, rhs: Point) -> Point {
+    return Point(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+}
+
+let a = Point(x: 3, y: 4)
+let b = Point(x: 3, y: 4)
+let abEqual = (a == b)
+let abNotEqual = (a != b)
+let c = Point(x: 2, y: 6)
+let d = Point(x: 3, y: 7)
+let cdEqual = (c == d)
+let cLessThanD = (c < d)
+let cLessThanEqualD = (c <= d)
+let cGreaterThanD = (c > d)
+let cGreaterThanEqualD = (c >= d)
+
+let e = a + b
+let f = c + d
+print(e)
+print(f)
+
