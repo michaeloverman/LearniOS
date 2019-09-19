8c752383c831a1cb9a30e7277f5cc5195eaf7589
Michael Overman <overman.michael@gmail.com>
2019-09-19 07:43:11 -0600

    iTahDoodle complete through Bronze challenges, ch 27

--- Swift/iTahDoodle/iTahDoodle/ViewController.swift
+++ Swift/iTahDoodle/iTahDoodle/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var itemTextField: UITextField!
+    @IBOutlet var tableView: UITableView!
+    
+    let todoList = TodoList()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = todoList
     }
 
+    @IBAction func addButtonPressed(sender: UIButton) {
+        print("Add to-do item: \(itemTextField.text ?? "_")")
+        guard let text = itemTextField.text else {
+            return
+        }
+        if text == "" { return }
+        todoList.addItem(item: text)
+        tableView.reloadData()
+        itemTextField.text = ""
+    }
 
 }
 
